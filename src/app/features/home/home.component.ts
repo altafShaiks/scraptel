2f4fac8e8fea94f468533441b80a3b874b3e1e59
Altaf Shaik <altaf.shaik@planonsoftware.com>
2025-09-15 21:03:32 +0530

    created what we accept cards

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, MatCardModule],
+  imports: [NavbarComponent, MatCardModule, MatChipsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })

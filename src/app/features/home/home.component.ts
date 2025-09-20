778cfba7524f306e89127c7a0f22b5775f14dbf7
Altaf Shaik <altaf.shaik@planonsoftware.com>
2025-09-20 06:35:44 +0530

    created footer and added in the home page

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, MatCardModule, MatChipsModule],
+  imports: [NavbarComponent, FooterComponent, MatCardModule, MatChipsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })

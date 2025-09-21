d11e5f7eb27283edc176e2fb8c5ec6325b5750af
Altaf Shaik <altaf.shaik@planonsoftware.com>
2025-09-21 08:46:35 +0530

    added icons and made the home page responsive

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, ViewChild } from '@angular/core';
+import { Component, HostListener, Input } from '@angular/core';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
 import { MatChipsModule } from '@angular/material/chips';
@@ -13,10 +13,16 @@ import { MatCardModule } from '@angular/material/card';
 })
 export class HomeComponent {
   isScrolled = false;
+  menuIsOpen: boolean = false;
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
     this.isScrolled = scrollTop > 10;
   }
+
+  onMenuToggled(isOpen: boolean) {
+  this.menuIsOpen = isOpen;
+  console.log('Menu opened:', this.menuIsOpen);
+}
 }
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,21 +1,30 @@
-import { Component, HostBinding, Input } from '@angular/core';
+import { Component, HostBinding, Input, Output, EventEmitter } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
   @Input() isScrolled = false;
-  menuOpened: boolean = false;
+  @Output() menuToggled = new EventEmitter<boolean>();
 
   @HostBinding('class.scrolled') get scrolledClass() {
     return this.isScrolled;
   }
 
+  menuOpened: boolean = false;
+
   openMenu() {
     this.menuOpened = true;
+    this.menuToggled.emit(this.menuOpened);
+  }
+
+  closeMenu() {
+    this.menuOpened = false;
+    this.menuToggled.emit(this.menuOpened);
   }
 
   setScrolled(value: boolean) {

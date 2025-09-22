3bcb395b51a779cdf372014af5ee75c437821467
Altaf Shaik <altaf.shaik@planonsoftware.com>
2025-09-22 09:36:04 +0530

    created register and login components as modals

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { AboutComponent } from './features/about/about.component';
 import { ContactComponent } from './features/contact/contact.component';
+import { LoginDialogComponent } from './features/auth/components/login/login-dialog.component';
+import { RegisterDialogComponent } from './features/auth/components/register/register-dialog.component';
 
 export const routes: Routes = [
     {
@@ -16,5 +18,13 @@ export const routes: Routes = [
     {
         path: 'contact',
         component: ContactComponent
+    },
+    {
+        path: 'login',
+        component: LoginDialogComponent
+    },
+    {
+        path: 'register',
+        component: RegisterDialogComponent
     }
 ];
